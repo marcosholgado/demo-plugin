15823ed25f4328ac3c6e35c12453525d661dce48
Marcos Holgado <marcos.holgadomolina@sky.uk>
2018-08-03 17:28:10 +0100

    Create Jira project component to hold the settings to connect to the API

---
+++ src/main/java/com/marcosholgado/droidcon18/plugin/configuration/JiraSettings.kt
@@ -0,0 +1,61 @@
+package com.marcosholgado.droidcon18.plugin.configuration
+
+import com.intellij.openapi.options.Configurable
+import com.intellij.openapi.project.Project
+import com.marcosholgado.droidcon18.plugin.components.JiraComponent
+import javax.swing.*
+import javax.swing.event.DocumentEvent
+import javax.swing.event.DocumentListener
+
+
+
+class JiraSettings(private val project: Project): Configurable, DocumentListener {
+    private var userField: JTextField? = null
+    private var passwordField: JPasswordField? = null
+    private var jiraURLField: JTextField? = null
+    private var userLabel: JLabel? = null
+    private var passwordLabel: JLabel? = null
+    private var jiraURLLabel: JLabel? = null
+    private var mainPanel: JPanel? = null
+
+    private var modified = false
+
+    override fun isModified(): Boolean = modified
+
+    override fun getDisplayName(): String = "Jira Plugin"
+
+    override fun apply() {
+        val config = JiraComponent.getInstance(project)
+        config.jiraUrl = jiraURLField!!.text
+        config.username = userField!!.text
+        config.password = passwordField!!.password.toString()
+
+        modified = false
+    }
+
+    override fun createComponent(): JComponent? {
+        jiraURLField?.document?.addDocumentListener(this)
+        userField?.document?.addDocumentListener(this)
+        passwordField?.document?.addDocumentListener(this)
+
+
+        val config = JiraComponent.getInstance(project)
+        userField?.text = config.username
+        passwordField?.text = config.password
+        jiraURLField?.text = config.jiraUrl
+
+        return mainPanel
+    }
+
+    override fun changedUpdate(e: DocumentEvent?) {
+        modified = true
+    }
+
+    override fun insertUpdate(e: DocumentEvent?) {
+        modified = true
+    }
+
+    override fun removeUpdate(e: DocumentEvent?) {
+        modified = true
+    }
+}
